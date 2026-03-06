d0de1df893c4ba5cf4839bbfef38f8d5fba45608
sergioga06 <sergiogallardog06@gmail.com>
2026-03-06 10:58:18 +0100

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 const geist = Geist({ subsets: ["latin"] })
