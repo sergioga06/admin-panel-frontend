56ba360fbd99aef9fc594c5c61b8805eabdff360
sergioga06 <sergiogallardog06@gmail.com>
2026-03-06 10:38:15 +0100

    commirt

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Forzamos la URL de la VPS. 
 // Asegúrate de que esta IP sea la de tu VPS
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://194.163.158.235:3000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://95.111.225.241:30080';
 
 const apiClient = axios.create({
   baseURL: API_URL,
